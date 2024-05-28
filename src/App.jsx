e47a1924c5c6eb39a3897a5e9c7d5fcaea23f074
ayan878 <ayanraza197@gmail.com>
2024-05-29 03:26:13 +0530

    App

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useColorMode, Button } from "@chakra-ui/react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Login from "./components/Login";
+import Orders from "./components/Orders";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { colorMode, toggleColorMode } = useColorMode();
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <Button
+        onClick={toggleColorMode}
+        position="absolute"
+        top="1rem"
+        right="1rem"
+      >
+        Toggle {colorMode === "light" ? "Dark" : "Light"}
+      </Button>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/orders/*" element={<Orders />} />
+        <Route path="*" element={<Navigate to="/login" />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
