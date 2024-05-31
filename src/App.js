1c9edef847cdd4c4b479720a2f519cbb50f6e421
ayan878 <ayanraza197@gmail.com>
2024-05-31 17:50:28 +0530

    app

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { Routes, Route, Navigate, Outlet } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import Dashboard from "./pages/Dashboard";
+import { useAuth } from "./hooks/useAuth";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/" element={<ProtectedRoute />}>
+        <Route path="/" element={<Dashboard />} />
+      </Route>
+    </Routes>
+  );
+};
+
+const ProtectedRoute = () => {
+  const { isAuthenticated } = useAuth();
+  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
+};
+
+export default App;
